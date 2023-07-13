11e0369b77d24057bd1045ada042abce5e57dc8d
Kgahlisho23 <kgahlishomaunye@gmail.com>
2023-07-13 11:18:41 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,10 +6,10 @@
  * Description - Serves as the starting point for program execution
  *
  * Return: Always 0 (Success)
- * */
- int main(void)
+ */
+int main(void)
 {
 	/*prints a string*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
