19e1662ad32a0c198f92275b7c952e0141cb108e
Kgahlisho23 <kgahlishomaunye@gmail.com>
2023-07-13 10:54:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
-{
-/**	
+{	
 	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
-*/	
+	return (0);	
 }	
