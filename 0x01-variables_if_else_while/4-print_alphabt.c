23dd09b2aa2d09bbada479e26651723cad8ba6ee
Kgahlisho23 <kgahlishomaunye@gmail.com>
2023-07-16 17:55:18 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - serves as the starting point for program execution
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (c != 'e' && c != 'q')
+			putchar (c);
+	}		
+	
+	putchar('\n');
+
+	return(0);
+}
